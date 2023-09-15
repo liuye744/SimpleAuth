1543f6ef60d7ec0c900be91219251a08940d26d1
CodingCube <15831262809@163.com>
2023-09-15 16:32:42 +0800

    更新文档

--- src/main/java/com/codingcube/simpleauth/SimpleAuthApplication.java
+++ src/main/java/com/codingcube/simpleauth/SimpleAuthApplication.java
@@ -1,9 +1,11 @@
 package com.codingcube.simpleauth;
 
+import com.codingcube.simpleauth.limit.annotation.EnableDynamicLimit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDynamicLimit
 public class SimpleAuthApplication {
     public static void main(String[] args) {
         SpringApplication.run(SimpleAuthApplication.class, args);
