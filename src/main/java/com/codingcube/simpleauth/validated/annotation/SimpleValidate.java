38d42877604fbfae02cf925d194659519f2456da
CodingCube <15831262809@163.com>
2023-11-14 21:18:08 +0800

    新权限校验方案(参数注解)

--- src/main/java/com/codingcube/simpleauth/validated/annotation/SimpleValidate.java
+++ src/main/java/com/codingcube/simpleauth/validated/annotation/SimpleValidate.java
@@ -2,7 +2,6 @@ package com.codingcube.simpleauth.validated.annotation;
 
 
 import com.codingcube.simpleauth.util.NullTarget;
-import com.codingcube.simpleauth.validated.strategic.DefaultValidateRejectedStratagem;
 import com.codingcube.simpleauth.validated.strategic.ValidateRejectedStratagem;
 
 import java.lang.annotation.ElementType;
@@ -11,9 +10,9 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.TYPE, ElementType.METHOD})
+@Target({ElementType.TYPE, ElementType.METHOD, ElementType.PARAMETER})
 public @interface SimpleValidate {
     Class<?> value() default Object.class;
-    String[] methods() default {"validate"};
+    String[] methods() default {};
     Class<? extends ValidateRejectedStratagem> rejected() default NullTarget.class;
 }
--- src/main/java/com/codingcube/simpleauth/validated/aspect/AutoValidated.java
+++ src/main/java/com/codingcube/simpleauth/validated/aspect/AutoValidated.java
@@ -8,19 +8,20 @@ import com.codingcube.simpleauth.validated.annotation.SimpleValidate;
 import com.codingcube.simpleauth.validated.domain.ReflectMethod;
 import com.codingcube.simpleauth.validated.strategic.ValidateRejectedStratagem;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.annotation.Resource;
+import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 @Aspect
 @Component
@@ -29,28 +30,94 @@ public class AutoValidated {
     ApplicationContext application;
 
     final Map<String, ReflectMethod> reflectMethodMap = new HashMap<>();
+    final Map<String, ReflectMethod> reflectParameterMap = new HashMap<>();
 
     @Around("@annotation(simpleValidate)")
     public Object validateMethod(ProceedingJoinPoint joinPoint, SimpleValidate simpleValidate) throws Throwable{
-        final String[] methodsString = simpleValidate.methods();
-        Class<?> validateObj = simpleValidate.value();
-        Class<? extends ValidateRejectedStratagem> rejected = simpleValidate.rejected();
+        String[] methodsString = simpleValidate.methods();
+        if (methodsString.length == 0){
+            methodsString = new String[1];
+            methodsString[0] = "validate";
+        }
+
         final SimpleValidate classAnnotation = joinPoint.getTarget().getClass().getAnnotation(SimpleValidate.class);
-        if (validateObj == Object.class){
+        final Class<?> validateObj = getValidateObjected(simpleValidate, classAnnotation);
+        final Class<? extends ValidateRejectedStratagem> rejected = getValidateRejected(simpleValidate, classAnnotation);
+
+        doPositiveMethodName(validateObj, methodsString, rejected, joinPoint);
+
+        return joinPoint.proceed();
+    }
+
+    @Around("execution(* *.*(.., @com.codingcube.simpleauth.validated.annotation.SimpleValidate (*), ..))")
+    public Object myAdvice(ProceedingJoinPoint joinPoint) throws Throwable{
+        //初始化参数
+        Signature signature = joinPoint.getSignature();
+        MethodSignature methodSignature = (MethodSignature) signature;
+        Method method = methodSignature.getMethod();
+        SimpleValidate simpleValidate = method.getAnnotation(SimpleValidate.class);
+
+        final SimpleValidate classAnnotation = joinPoint.getTarget().getClass().getAnnotation(SimpleValidate.class);
+        final Class<?> validateObj = getValidateObjected(simpleValidate, classAnnotation);
+        final Class<? extends ValidateRejectedStratagem> rejected = getValidateRejected(simpleValidate, classAnnotation);
+
+
+        //查询参数是否携带SimpleValidate注解
+        final Annotation[][] parameterAnnotations = method.getParameterAnnotations();
+        final Object[] args = joinPoint.getArgs();
+        for (int i = 0; i < parameterAnnotations.length; i++) {
+            Annotation[] parameterAnnotation = parameterAnnotations[i];
+            for (Annotation annotation : parameterAnnotation) {
+                if (annotation.annotationType() == SimpleValidate.class){
+                    final String[] methodsString = ((SimpleValidate) annotation).methods();
+                    //存在methodsString 执行positiveMethod
+                    if (methodsString.length != 0){
+                        doPositiveMethodName(validateObj, methodsString, rejected, joinPoint);
+                        return joinPoint.proceed();
+                    }
+                    final Object targetParameter = args[i];
+                    //不存在MethodsString 执行positiveParameter
+
+                    doPositiveParameter(validateObj, rejected, targetParameter);
+                }
+            }
+        }
+
+        return joinPoint.proceed();
+    }
+
+    private Class<?> getValidateObjected(SimpleValidate simpleValidate,SimpleValidate fatherSimpleValidate){
+        Class<?> validateObj;
+        if (simpleValidate == null || (validateObj=simpleValidate.value()) == Object.class){
             //寻找类上的validateObj
-            if (classAnnotation == null || (validateObj = classAnnotation.value()) == Object.class ){
+            if (fatherSimpleValidate == null || (validateObj = fatherSimpleValidate.value()) == Object.class ){
                 //全局ValidatedObjected
                 if ((validateObj = ValidateProper.getDefaultValidateObjectClazz()) == Object.class){
                     throw new ValidateMethodException("Requires a validate class as the value parameter, which can be placed on method or class annotations");
                 }
             }
         }
-        if (rejected == NullTarget.class){
-            if (classAnnotation == null || (rejected = classAnnotation.rejected()) == NullTarget.class ){
+        return validateObj;
+    }
+
+    private Class<? extends ValidateRejectedStratagem> getValidateRejected(SimpleValidate simpleValidate,SimpleValidate fatherSimpleValidate){
+        Class<? extends ValidateRejectedStratagem> rejected;
+        if (simpleValidate==null || (rejected=simpleValidate.rejected()) == NullTarget.class){
+            if (fatherSimpleValidate == null || (rejected = fatherSimpleValidate.rejected()) == NullTarget.class ){
                 rejected = ValidateProper.getDefaultRejectedClazz();
             }
         }
+        return rejected;
+    }
 
+    /**
+     * * 存在确定的方法名时执行
+     * @param validateObj 验证类
+     * @param methodsString 方法名
+     * @param rejected 拒绝策略
+     * @param joinPoint 方法切点
+     */
+    private void doPositiveMethodName(Class<?> validateObj, String[] methodsString, Class<? extends ValidateRejectedStratagem> rejected, ProceedingJoinPoint joinPoint) throws Throwable {
         for (String methodName : methodsString) {
             final String key = key(validateObj, methodName);
             ReflectMethod reflectMethod = reflectMethodMap.get(key);
@@ -93,10 +160,53 @@ public class AutoValidated {
                 reflectMethod = reflectMethod.getNext();
             }
         }
-
-        return joinPoint.proceed();
     }
 
+    /**
+     * 存在确定的参数时执行*
+     * @param validateObj 实力类型
+     * @param rejected 拒绝策略
+     * @param parameter 参数
+     */
+    private void doPositiveParameter(Class<?> validateObj, Class<? extends ValidateRejectedStratagem> rejected, Object parameter) throws Throwable {
+        //判断并初始化缓存
+        final String parameterKey = parameterKey(validateObj, parameter.getClass());
+        ReflectMethod reflectMethod = reflectParameterMap.get(parameterKey);
+        if (reflectMethod == null){
+            initReflectParameterCache(parameterKey, validateObj, parameter.getClass());
+            reflectMethod = reflectParameterMap.get(parameterKey);
+        }
+        while (reflectMethod.getNext() != null){
+            final Method method = reflectMethod.getMethod();
+            final Object instance = reflectMethod.getInstance();
+
+            try{
+                final Object validatedResult = method.invoke(instance, parameter);
+                if (validatedResult instanceof Boolean){
+                    if (!(Boolean) validatedResult){
+                        //验证失败
+                        final ValidateRejectedStratagem rejectedInstance = AuthHandlerUtil.getBean(application, rejected);
+                        final ServletRequestAttributes attributes = (ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes());
+                        rejectedInstance.doRejected(attributes.getRequest(), attributes.getResponse(), parameter);
+                    }
+                }else {
+                    throw new ValidateMethodException("he return value type of "+ method.getName() +" should be Boolean.");
+                }
+
+            } catch (InvocationTargetException e) {
+                throw e.getCause();
+            }
+            reflectMethod = reflectMethod.getNext();
+        }
+    }
+
+
+    /**
+     * 初始化确定方法名的反射缓存*
+     * @param key 缓存key
+     * @param validateObj 缓存实例类型
+     * @param methodName 方法名
+     */
     private void initReflectMethodCache(String key, Class<?> validateObj, String methodName){
         synchronized (getReflectMethodCacheMapMutex()){
             //初始化
@@ -105,17 +215,7 @@ public class AutoValidated {
                 final Object bean = AuthHandlerUtil.getBean(application, validateObj);
                 for (Method method : methods) {
                     if (method.getParameterCount() == 1 && method.getName().equals(methodName)){
-                        method.setAccessible(true);
-                        final ReflectMethod newReflectMethod = new ReflectMethod(bean, method, method.getParameterTypes()[0]);
-                        if (reflectMethodMap.get(key) == null){
-                            reflectMethodMap.put(key, newReflectMethod);
-                        }else {
-                            ReflectMethod mapReflectMethod = reflectMethodMap.get(key);
-                            while (mapReflectMethod.getNext() != null){
-                                mapReflectMethod = mapReflectMethod.getNext();
-                            }
-                            mapReflectMethod.setNext(newReflectMethod);
-                        }
+                        updateReflectMap(key, bean, method, reflectMethodMap);
                     }
                 }
             }
@@ -123,11 +223,81 @@ public class AutoValidated {
 
     }
 
+    /**
+     * 初始化确定参数类型的反射缓存*
+     * @param key 缓存key
+     * @param validateObj 缓存实例类型
+     * @param parameter 缓存参数类型(仅缓存存在一个实例的对象)
+     */
+    private void initReflectParameterCache(String key, Class<?> validateObj, Class<?> parameter){
+        synchronized (getReflectParameterCacheMapMutex()){
+            if(reflectParameterMap.get(key) == null){
+                final Method[] methods = validateObj.getMethods();
+                final Object bean = AuthHandlerUtil.getBean(application, validateObj);
+                for (Method method : methods) {
+                    if (method.getParameterCount() == 1 && method.getParameterTypes()[0] == parameter){
+                        updateReflectMap(key, bean, method, reflectParameterMap);
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * 更新反射缓存
+     * @param key 缓存key
+     * @param bean 应用实例
+     * @param method 反射方法
+     * @param reflectMap 缓存Map
+     */
+    private void updateReflectMap(String key, Object bean, Method method, Map<String, ReflectMethod> reflectMap) {
+        method.setAccessible(true);
+        final ReflectMethod newReflectMethod = new ReflectMethod(bean, method, method.getParameterTypes()[0]);
+        if (reflectMap.get(key) == null){
+            reflectMap.put(key, newReflectMethod);
+        }else {
+            ReflectMethod mapReflectMethod = reflectMap.get(key);
+            while (mapReflectMethod.getNext() != null){
+                mapReflectMethod = mapReflectMethod.getNext();
+            }
+            mapReflectMethod.setNext(newReflectMethod);
+        }
+    }
+
     private String key(Class<?> validateObjClazz, String methodName){
         return validateObjClazz.getName() + "$" + methodName;
     }
 
+    private String parameterKey(Class<?> validateObjClazz, Class<?> parameter){
+        return validateObjClazz.getName() + "$" + parameter.getName();
+    }
+
+    /**
+     * 获取添加有指定注解的参数列表*
+     * @param method 方法
+     * @param joinPoint 切点
+     * @param annotationClass 注解类型
+     * @return 参数列表
+     */
+    private Object[] getParameterContainAnnotation(Method method,ProceedingJoinPoint joinPoint, Class<?> annotationClass){
+        List<Object> objects = new ArrayList<>();
+//        final Annotation[][] parameterAnnotations = method.getParameterAnnotations();
+//        final Object[] args = joinPoint.getArgs();
+//        for (int i = 0; i < parameterAnnotations.length; i++) {
+//            Annotation[] parameterAnnotation = parameterAnnotations[i];
+//            for (Annotation annotation : parameterAnnotation) {
+//                if (annotation.annotationType() == annotationClass){
+//                    objects.add(args[i]);
+//                }
+//            }
+//        }
+        return objects.toArray();
+    }
+
     private Object getReflectMethodCacheMapMutex(){
         return reflectMethodMap;
+    }
+    private Object getReflectParameterCacheMapMutex(){
+        return reflectParameterMap;
     }
 }
